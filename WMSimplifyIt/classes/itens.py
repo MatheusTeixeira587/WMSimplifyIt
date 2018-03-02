a68cac3d1c4c8f523995de0ebc0b7e518cd979c2
MatheusTeixeira587 <33670784+MatheusTeixeira587@users.noreply.github.com>
2018-03-01 22:11:36 -0300

    new functions added

---
+++ WMSimplifyIt/classes/itens.py
@@ -0,0 +1,50 @@
+import sqlite3
+
+class Itens():
+
+    def __init__(self,nome = "",table = "item",quantity = "",price = "", address = "a00b00"):
+
+        self.nome = nome
+        self.table = table
+        self.quantity = quantity
+        self.price = price
+        self.address = address
+        self.rua = address[0]
+        self.prateleira = address[1:3]
+        self.andar = address[3]
+        self.posicao = address[4:6]
+
+    def add_new(self,nome,quantity,price):
+        conn = sqlite3.connect("projetowms.db")
+        cur = conn.cursor()
+        cur.execute("INSERT INTO " + self.table + " (nome,quantity,price) VALUES (?,?,?)",(nome,quantity,price))
+        conn.commit()
+        conn.close()
+
+    def view_all(self):
+        conn = sqlite3.connect("projetowms.db")
+        cur = conn.cursor()
+        dados = cur.execute("SELECT * FROM " + self.table).fetchall()
+        conn.close()
+        return dados
+
+    def view_one(self,_id_):
+        conn = sqlite3.connect("projetowms.db")
+        cur = conn.cursor()
+        dados = cur.execute("SELECT * FROM " + self.table + " WHERE id = ?",(_id_,)).fetchone()
+        conn.close()
+        return dados
+
+    def update(self,nome,quantity,price,_id_):
+        conn = sqlite3.connect("projetowms.db")
+        cur = conn.cursor()
+        cur.execute("UPDATE " + self.table + " SET nome = ? quantity = ? price = ? WHERE id = ?",(nome,quantity,price,_id_))
+        conn.commit()
+        conn.close()
+
+    def delete(self,_id_):
+        conn = sqlite3.connect("projetowms.db")
+        cur = conn.cursor()
+        cur.execute("DELETE FROM " + self.table + " WHERE id = ?",(_id_,))
+        conn.commit()
+        conn.close()
---
+++ WMSimplifyIt/routes/funcio_routes.py
@@ -0,0 +1,84 @@
+from flask import *
+from classes.funcionario import User
+import bcrypt
+blue_funcio = Blueprint("func",__name__,template_folder = "templates")
+
+@blue_funcio.route("/login", methods = ["GET","POST"])
+def login_funcio(dados = ""):
+    if request.method == "GET":
+        return render_template("login_funcio.html")
+    else:
+        nome = request.form["nome"]
+        senha = request.form["senha"]
+        u1 = User()
+        if(u1.check_pass(nome,senha)):
+            session["User"] = nome
+            if dados == "":
+                dados = "/"
+            return redirect(dados)
+        else:
+            return redirect("/funcionarios/login")
+
+@blue_funcio.route("/add_funcio", methods = ["GET","POST"])
+def add_funcio():
+    u1 = User()
+    if "User" in session:
+        if request.method == "GET":
+            return render_template("add_funcio.html")
+        else:
+            nome = request.form["nome"]
+            senha = bcrypt.hashpw(request.form["senha"].encode("utf-8"),bcrypt.gensalt())
+            u1.add_new(nome,senha)
+            return redirect("/funcionarios/viewall_funcio")
+    else:
+        return redirect("/funcionarios/login")
+
+@blue_funcio.route("/update_funcio/<_id_>", methods = ["GET","POST"])
+def update_funcio(_id_):
+    u1 = User()
+    if "User" in session:
+        if request.method == "GET":
+            return render_template("update_funcio.html", dados = u1.view_one(_id_))
+        else:
+            nome = request.form["nome"]
+            antiga_senha = request.form["antiga_senha"]
+            nova_senha = request.form["senha"]
+            confirma_nova_senha = request.form["senha2"]
+            if nova_senha == confirma_nova_senha:
+                u1 = User()
+                if(u1.check_pass(_id_,antiga_senha)):
+                    salt = bcrypt.gensalt()
+                    senha = bcrypt.hashpw(nova_senha.encode("utf-8"),salt)
+                    u1.update(nome,_id_,senha)
+                    return redirect("/funcionarios/viewone_funcio/" + _id_ + "")
+    else:
+        return redirect("/funcionarios/login")
+
+
+@blue_funcio.route("/delete_funcio/<_id_>", methods = ["GET", "POST"])
+def delete_funcio(_id_):
+    u1 = User()
+    if "User" in session:
+        if request.method == "GET":
+            return render_template("delete_funcio.html", dados = u1.view_one(_id_))
+        else:
+            u1.delete(_id_)
+            return redirect("/funcionarios/viewall_funcio")
+    else:
+        return redirect("funcionarios/login")
+
+@blue_funcio.route("/viewall_funcio", methods = ["GET","POST"])
+def viewall_funcio():
+    u1 = User()
+    if "User" in session:
+        return render_template("viewall_funcio.html", dados = u1.view_all())
+    else:
+        return redirect("/funcionarios/login")
+
+@blue_funcio.route("/viewone_funcio/<_id_>", methods = ["GET"])
+def viewone_funcio(_id_):
+    u1 = User()
+    if "User" in session:
+        return render_template("viewone_funcio.html", dados = u1.view_one(_id_))
+    else:
+        return redirect("/funcionarios/login")
